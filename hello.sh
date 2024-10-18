1c542dc0538daa352705b0943401c65d83727291
bhargesh546 <84077491+bhargesh546@users.noreply.github.com>
2024-10-18 19:26:25 +0000

    final

--- hello.sh
+++ hello.sh
@@ -3,3 +3,5 @@
 echo "hello world"
 
 # chmod +x hello.sh - is used to make it executable
+
+# sudo -s  for gaining/switching to root superuser 
---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+echo "Bye bye :-) "  
+
+# create a link
+# ln -s poems.txt writing.txt   relative/symbolic link
+# While the rmdir command can only delete empty directories, 
+# you can use rm -rf to delete non-empty directories and trees of directories.
